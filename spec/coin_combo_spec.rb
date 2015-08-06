bcc9000d09d9a1e839e9c48d01a9f8ec2723dc98
alex altieri & christian lei <student@epicodus.com>
2015-08-06 11:05:30 -0700

    Passes nickels spec

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -9,4 +9,8 @@ describe('Fixnum#coin_combo') do
   it('divides change into dimes') do
     expect(10.coin_combo()).to(eq("You will need 0 quarter(s), 1 dime(s), 0 nickel(s), 0 penn(y/ies)."))
   end
+
+  it('divides change into nickels') do
+    expect(5.coin_combo()).to(eq("You will need 0 quarter(s), 0 dime(s), 1 nickel(s), 0 penn(y/ies)."))
+  end
 end
