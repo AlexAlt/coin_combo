f3bf4d5ebee4b4635ee06e5761c1f6b24d06e394
alex altieri & christian lei <student@epicodus.com>
2015-08-06 14:24:08 -0700

    adds a nonworking integration spec

--- spec/coin_combo_integration_spec.rb
+++ spec/coin_combo_integration_spec.rb
@@ -1,14 +1,13 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
-set (:show_exceptions , false)
+set(:show_exceptions, false)
 
 describe('the coin combo path' , {:type => :feature}) do
   it('processes user entry and returns coin combination') do
     visit('/')
-    fill_in('coin_combo', :with '4523')
-    click_button("Enter")
+    fill_in("money_input" :with "4523")
+    click_button('Enter')
     expect(page).to have_content("You will need 180 quarter(s), 2 dime(s), 0 nickel(s), 3 penn(y/ies).")
   end
-
 end
