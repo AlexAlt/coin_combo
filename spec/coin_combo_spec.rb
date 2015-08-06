04ec3dcb38f15eb1ffd7da220a85f0f79cbe29fb
alex altieri & christian lei <student@epicodus.com>
2015-08-06 11:06:20 -0700

    Passes final spec for pennies

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -13,4 +13,8 @@ describe('Fixnum#coin_combo') do
   it('divides change into nickels') do
     expect(5.coin_combo()).to(eq("You will need 0 quarter(s), 0 dime(s), 1 nickel(s), 0 penn(y/ies)."))
   end
+
+  it('divides change into quarters') do
+    expect(1.coin_combo()).to(eq("You will need 0 quarter(s), 0 dime(s), 0 nickel(s), 1 penn(y/ies)."))
+  end
 end
